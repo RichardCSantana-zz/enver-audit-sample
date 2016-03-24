2b7a859e19b886002f3de42a3d321df8392c100a
RichardCSantana <richardcsantana@gmail.com>
2016-03-24 17:29:52 -0300

    ajuste no modelo de gravação do log

--- src/main/java/com/richardcsantana/model/UserRevEntity.java
+++ src/main/java/com/richardcsantana/model/UserRevEntity.java
@@ -4,8 +4,13 @@ import com.richardcsantana.audit.UserRevisionListener;
 import lombok.Data;
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
 
 /**
  * Created by richardsantana on 24/03/16.
@@ -13,8 +18,16 @@ import javax.persistence.Entity;
 @Entity
 @RevisionEntity(UserRevisionListener.class)
 @Data
-public class UserRevEntity extends DefaultRevisionEntity {
+public class UserRevEntity {
+
+    @Id
+    @GeneratedValue
+    @RevisionNumber
+    private Long id;
 
     private String username;
+    
+    @RevisionTimestamp
+    private Date date;
 
 }
